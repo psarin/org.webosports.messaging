030d045f4f54147cb37170c22b76f950b3157961
Pankaj Sarin <psarin1@partners.org>
2015-01-29 10:15:03 +0100

    Styling changes

--- app/source/package.js
+++ app/source/package.js
@@ -1,8 +1,8 @@
 enyo.depends(
     "$lib/layout",
    // "$lib/layout/flex/source",
-    "$lib/onyx",    // To theme Onyx using Theme.less, change this line to $lib/onyx/source,
-    //"Theme.less",    // uncomment this line, and follow the steps described in Theme.less
+    "$lib/onyx/source",    // To theme Onyx using Theme.less, change this line to $lib/onyx/source,
+    "style/Theme.less",    // uncomment this line, and follow the steps described in Theme.less
     //"$lib/webos-lib",
     "$lib/enyo-webos",
     "$lib/webos-lib/source/BackGesture.js",
